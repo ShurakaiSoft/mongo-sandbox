74c9f4fd94332d21c8be53fab9e5b1a94b25230a
Stephen Parker <ShurakaiSoft@gmail.com>
2013-07-22 15:51:17 +1000

    Added age validation

--- data/schemas/user.js
+++ data/schemas/user.js
@@ -5,8 +5,12 @@
 var mongoose = require('mongoose');
 
 var emailRegex = /.+\@.+\..+/; // incomplete
+var yearInSeconds = 31536000000;
+var thirteenYears = yearInSeconds * 13;
 
-
+function validate13YearsOldOrMore(date) {
+	return (Date.now() - date.getTime()) > thirteenYears;
+}
 
 var UserSchema = new mongoose.Schema({
 	username: { type: String, unique: true },
@@ -22,6 +26,12 @@ var UserSchema = new mongoose.Schema({
 		required: true,
 		uppercase: true,
 		'enum': ['M', 'F']
+	},
+	birthday: {
+		type: Date,
+		validate: [
+		    validate13YearsOldOrMore,
+		    'you must be 13 years old or more']
 	}
 });
 
